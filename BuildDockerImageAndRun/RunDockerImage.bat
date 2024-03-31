c332b53e34dea4e05c64cf6909b46ddc48bfc072
StevenY <zebrakerker@icloud.com>
2024-03-31 13:55:52 -0700

    Add description in docker run batch file.

--- BuildDockerImageAndRun/RunDockerImage.bat
+++ BuildDockerImageAndRun/RunDockerImage.bat
@@ -1,2 +1,3 @@
-@REM docker run -it steven/amd64_ubuntu20.4 /bin/bash  => this command doesn't have a shared folder between host tool and the container.
+@REM docker run -it steven/amd64_ubuntu20.4 /bin/bash  => this command doesn't have a shared folder between host machine and the container.
+@REM "-v C:\Users\top90\Desktop\BuildMyGit:/shared" means to share host machine directory C:\Users\top90\Desktop\BuildMyGit with container directory /shared
 docker run -it -v C:\Users\top90\Desktop\BuildMyGit:/shared steven/amd64_ubuntu20.4 /bin/bash
